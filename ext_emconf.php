d8c0b71eeb3c96402e5793c222cb036727dcb06a
Philipp Kitzberger <coding@kitze.net>
2023-09-17 10:31:22 +0200

    Bumped version constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,37 +1,35 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'CLI toolbox',
-	'description' => '',
-	'category' => 'system',
-	'shy' => 0,
-	'dependencies' => 'cms',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
-	'state' => 'stable',
-	'internal' => 0,
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 1,
-	'lockType' => '',
-	'author' => 'Philipp Kitzberger',
-	'author_email' => 'philipp@kitze.net',
-	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-	'version' => '1.0.2',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '6.2.0-8.99.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'suggests' => array(
-	),
-);
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'CLI toolbox',
+    'description' => '',
+    'category' => 'system',
+    'shy' => 0,
+    'dependencies' => 'cms',
+    'conflicts' => '',
+    'priority' => '',
+    'loadOrder' => '',
+    'module' => '',
+    'state' => 'stable',
+    'internal' => 0,
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 1,
+    'lockType' => '',
+    'author' => 'Philipp Kitzberger',
+    'author_email' => 'typo3@kitze.net',
+    'author_company' => '',
+    'version' => '1.0.2',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.4.0-11.5.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+    'suggests' => [
+    ],
+];
