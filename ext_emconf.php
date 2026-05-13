56eefdc3cb5dfc3f4752781a386a2d112e3a0f77
Philipp Kitzberger <coding@kitze.net>
2026-05-13 09:01:35 +0200

    Raise version constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '4.2.6',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-13.4.99',
+            'typo3' => '10.4.0-14.3.99',
         ],
         'conflicts' => [
         ],
